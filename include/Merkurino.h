44b22a1a7309cbb2906bba9db70097db4319eac5
Jan Koupil <jan.koupil@gmail.com>
2022-06-04 19:58:07 +0200

    Update naming convention

--- include/Merkurino.h
+++ include/Merkurino.h
@@ -5,20 +5,23 @@
 #include <Wire.h>
 #include <LiquidCrystal.h>
 
+#define MERKURINO_BUZZER_PIN A3;
+#define MERKURINO_KBD_PIN A0;
+
 class Merkurino {
     
   public:
 
     enum Button {None, Up, Down, Left, Right, Enter, Escape};
 
-    LiquidCrystal *lcd = new LiquidCrystal(13, 12, 8, 7, 4, 2);    
+    LiquidCrystal *lcd = new LiquidCrystal(13, 12, 8, 7, 4, 2);
     
     Merkurino();
 
-    void InitKbdPin();
-    void InitBuzzerPin();    
-    Merkurino::Button ReadKbd();
-    void Buzzer(bool on);
+    void initKbdPin();
+    void initBuzzerPin();
+    Merkurino::Button readKbd();
+    void buzzer(bool on);
 
 };
 
--- src/Merkurino.cpp
+++ src/Merkurino.cpp
@@ -4,16 +4,16 @@ Merkurino::Merkurino(){
     lcd->begin(16, 2);
 };
 
-void Merkurino::InitKbdPin(){
-    pinMode(A0, INPUT);
+void Merkurino::initKbdPin(){
+    pinMode(MERKURINO_KBD_PIN, INPUT);
 };
 
-void Merkurino::InitBuzzerPin(){
-    pinMode(A3, OUTPUT);
+void Merkurino::initBuzzerPin(){
+    pinMode(MERKURINO_BUZZER_PIN, OUTPUT);
 }
     
-Merkurino::Button Merkurino::ReadKbd(){
-    int adc_key_in = analogRead(A0); // čtení "napětí" na vstupu A0
+Merkurino::Button Merkurino::readKbd(){
+    int adc_key_in = analogRead(MERKURINO_KBD_PIN); // čtení "napětí" na vstupu A0
     
     if (adc_key_in < 40) return Merkurino::Button::Escape; 
     else if (adc_key_in < 110) return Merkurino::Button::Left;
@@ -24,7 +24,7 @@ Merkurino::Button Merkurino::ReadKbd(){
     else return Merkurino::Button::None;
 };
 
-void Merkurino::Buzzer(bool on){
-    digitalWrite(A3, on ? HIGH : LOW);      
+void Merkurino::buzzer(bool on){
+    digitalWrite(MERKURINO_BUZZER_PIN, on ? HIGH : LOW);      
 }
 
