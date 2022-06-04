a8b1bf9c893d5910bac449513d3a38ee0b11664f
Jan Koupil <jan.koupil@gmail.com>
2022-06-04 22:43:56 +0200

    Add merkurino input library

---
+++ include/MerkurinoInput.h
@@ -0,0 +1,56 @@
+#ifndef MerkurinoInput_H
+#define MerkurinoInput_H
+
+#include <Arduino.h>
+#include <Merkurino.h>
+#include <SimpleTimer.h>
+
+#define MERKURINO_DEFAULT_KEY_DELAY 200
+
+class MerkurinoInput
+{
+private:
+    Merkurino merkurino_;
+    SimpleTimer keyTimer_;
+    
+    char symbolList_[128];
+    int symbolCnt_;
+
+    int inputStartCol_;
+    int inputStartRow_;
+
+    int cursorX_;
+    int inputLength_;
+    char initialText_[16];
+    char currentText_[16];
+    
+    bool isReady_;
+    Merkurino::Button pressedBtn_;
+
+    void initDisplay();
+    void displayCurrentSymbol();
+    int findSymbolIndex(char symbol);
+    void changeSymbol(int shift);
+    void moveCursor(int shift);
+    void submit();
+
+public:
+    MerkurinoInput(Merkurino m);
+    
+    bool isReady();
+    void startReading(char defaultText[], int inputLength);
+    void startReading(String defaultText);
+    void resetInput();
+    String getInput();
+    void readKeys();
+    void handleKeyPress();
+    void setKeyDelay(unsigned int delay);
+
+    void useSymbols(char symbolList[], int count);
+    void useNumberChars();
+    void useLowercaseChars();
+    void useUppercaseChars();
+    void useStandardChars();
+};
+
+#endif
---
+++ src/MerkurinoInput.cpp
@@ -0,0 +1,241 @@
+#include <MerkurinoInput.h>
+
+void MerkurinoInput::initDisplay()
+{
+    for (int i = 0; i < inputLength_; i++)
+    {
+        cursorX_ = i;
+        displayCurrentSymbol();
+    }
+    cursorX_ = 0;
+    merkurino_.lcd.setCursor(inputStartCol_, inputStartRow_);
+    merkurino_.lcd.blink();
+};
+
+void MerkurinoInput::displayCurrentSymbol()
+{
+    merkurino_.lcd.setCursor(inputStartCol_ + cursorX_, inputStartRow_);
+    merkurino_.lcd.print(currentText_[cursorX_]);
+    merkurino_.lcd.setCursor(inputStartCol_ + cursorX_, inputStartRow_);
+};
+
+int MerkurinoInput::findSymbolIndex(char symbol)
+{
+    for (int i = 0; i < symbolCnt_; i++)
+    {
+        if (symbol == symbolList_[i])
+            return i;
+    }
+    return 0;
+};
+
+void MerkurinoInput::changeSymbol(int shift)
+{
+    int index = findSymbolIndex(currentText_[cursorX_]);
+    index += shift;
+    while (index >= symbolCnt_)
+        index -= symbolCnt_;
+    while (index < 0)
+        index += symbolCnt_;
+    currentText_[cursorX_] = symbolList_[index];
+    displayCurrentSymbol();
+};
+
+void MerkurinoInput::moveCursor(int shift)
+{
+    cursorX_ += shift;
+    if (cursorX_ >= inputLength_)
+        cursorX_ = inputLength_ - 1;
+    else if (cursorX_ < 0)
+        cursorX_ = 0;
+};
+
+void MerkurinoInput::submit()
+{
+    isReady_ = true;
+    merkurino_.lcd.noBlink();
+    keyTimer_.stop();
+};
+
+MerkurinoInput::MerkurinoInput(Merkurino m)
+{
+    merkurino_ = m;
+    inputStartCol_ = 0;
+    inputStartRow_ = 1;
+    setKeyDelay(MERKURINO_DEFAULT_KEY_DELAY);
+};
+
+bool MerkurinoInput::isReady()
+{
+    return isReady_;
+};
+
+void MerkurinoInput::startReading(char defaultText[], int inputLength)
+{
+    this->inputLength_ = inputLength;
+
+    for (int i = 0; i < inputLength; i++)
+    {
+        initialText_[i] = defaultText[i];
+    }
+
+    resetInput();
+};
+
+void MerkurinoInput::startReading(String defaultText)
+{
+    this->inputLength_ = defaultText.length();
+
+    for (int i = 0; i < inputLength_; i++)
+    {
+        initialText_[i] = defaultText[i];
+    }
+
+    resetInput();
+};
+
+void MerkurinoInput::resetInput()
+{
+    this->cursorX_ = 0;
+    this->isReady_ = false;
+
+    for (int i = 0; i < inputLength_; i++)
+    {
+        currentText_[i] = initialText_[i];
+    }
+
+    merkurino_.initKbdPin();
+    keyTimer_.start();
+    pressedBtn_ = Merkurino::Button::None;
+    initDisplay();
+};
+
+String MerkurinoInput::getInput()
+{
+    String inputText = "";
+    for (int i = 0; i < inputLength_; i++)
+    {
+        inputText += currentText_[i];
+    }
+    return inputText;
+};
+
+void MerkurinoInput::readKeys()
+{
+    if (isReady_)
+        return;
+
+    Merkurino::Button pressedBtn = merkurino_.readKbd();
+    if (pressedBtn != Merkurino::Button::None)
+        pressedBtn_ = pressedBtn;
+
+    if (keyTimer_.hasTicked())
+        handleKeyPress();
+};
+
+void MerkurinoInput::handleKeyPress()
+{
+    switch (merkurino_.readKbd())
+    {
+    case Merkurino::Button::Up:
+        changeSymbol(+1);
+        break;
+
+    case Merkurino::Button::Down:
+        changeSymbol(-1);
+        break;
+
+    case Merkurino::Button::Left:
+        moveCursor(-1);
+        break;
+
+    case Merkurino::Button::Right:
+        moveCursor(+1);
+        break;
+
+    case Merkurino::Button::Enter:
+        submit();
+        break;
+
+    case Merkurino::Button::Escape:
+        resetInput();
+        break;
+
+    default:
+        break;
+    }
+
+    pressedBtn_ = Merkurino::Button::None;
+    displayCurrentSymbol();
+};
+
+void MerkurinoInput::setKeyDelay(unsigned int delay) // milliseconds
+{
+    keyTimer_.setInterval(delay);
+};
+
+void MerkurinoInput::useSymbols(char symbolList[], int count)
+{
+    for (int i = 0; i < count; i++)
+        symbolList[i] = symbolList[i];
+
+    symbolCnt_ = count;
+};
+
+void MerkurinoInput::useNumberChars()
+{
+    char symbol = '0';
+
+    for (int i = 0; i < 10; i++)
+        symbolList_[i] = symbol++;
+
+    symbolCnt_ = 10;
+};
+
+void MerkurinoInput::useLowercaseChars()
+{
+    symbolList_[0] = ' ';
+
+    char symbol = 'a';
+
+    for (int i = 0; i < 26; i++)
+        symbolList_[i + 1] = symbol++;
+
+    symbolCnt_ = 27;
+};
+
+void MerkurinoInput::useUppercaseChars()
+{
+    symbolList_[0] = ' ';
+
+    char symbol = 'A';
+
+    for (int i = 0; i < 26; i++)
+        symbolList_[i + 1] = symbol++;
+
+    symbolCnt_ = 27;
+};
+
+void MerkurinoInput::useStandardChars()
+{
+    symbolList_[0] = ' ';
+    int i = 1;
+    char symbol;
+
+    symbol = 'A';
+    for (int j = 0; j < 26; i++, j++)
+        symbolList_[i] = symbol++;
+
+    symbol = 'a';
+    for (int j = 0; j < 26; i++, j++)
+        symbolList_[i] = symbol++;
+
+    symbol = '0';
+    for (int j = 0; j < 10; i++, j++)
+        symbolList_[i] = symbol++;
+
+    symbolList_[i++] = '-';
+    symbolList_[i++] = '_';
+
+    symbolCnt_ = i;
+};
